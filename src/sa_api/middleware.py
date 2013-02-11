3162351f4118e8d967c8dd86035bcc0dd28537d8
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-02-11 12:37:05 -0500

    Protect RequestTimeLogger.process_response against edge cases.

--- src/sa_api/middleware.py
+++ src/sa_api/middleware.py
@@ -6,15 +6,19 @@ class RequestTimeLogger (object):
         self.start_time = time.time()
 
     def process_response(self, request, response):
-        duration = time.time() - self.start_time
+        # NOTE: If there was some exception, or some other reason that the
+        # process_request method was not called, we won't know the start time.
+        # Check that we know it first.
+        if hasattr(self, 'start_time'):
+            duration = time.time() - self.start_time
 
-        # Log the time information
-        logger = logging.getLogger('utils.request_timer')
-        logger.debug('(%0.3f) "%s %s" %s' % (
-            duration,
-            request.method,
-            request.get_full_path(),
-            response.status_code
-        ))
+            # Log the time information
+            logger = logging.getLogger('utils.request_timer')
+            logger.debug('(%0.3f) "%s %s" %s' % (
+                duration,
+                request.method,
+                request.get_full_path(),
+                response.status_code
+            ))
 
         return response
