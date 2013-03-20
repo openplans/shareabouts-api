f0573561ed8fffaf090d0fb762b2a03475dfd414
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-03-20 00:05:14 -0400

    Update the status of Django REST Framework
    - Remove the special version github repo version from the requirements
    - Tell Django where to find the version in the libs folder

--- src/manage.py
+++ src/manage.py
@@ -2,7 +2,13 @@
 import os
 import sys
 
+from os.path import abspath, dirname, join, pardir
+
 if __name__ == "__main__":
+    # Include the old Django REST Framework from the libs path.  See the note
+    # for commit 8a9c3078826 for more information.
+    sys.path.append(abspath(join(dirname(__file__), pardir, 'libs', 'django-rest-framework-0.4')))
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
     from django.core.management import execute_from_command_line
