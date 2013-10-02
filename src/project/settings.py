f51274288c8277c64af3d633ade849326f529879
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-10-02 09:39:46 -0400

    Specify the debug toolbar panels with cache debugging enabled

--- src/project/settings.py
+++ src/project/settings.py
@@ -194,7 +194,18 @@ DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False
 }
 INTERNAL_IPS = ('127.0.0.1',)
-
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.cache.CacheDebugPanel',  # Disabled by default
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
 ################################################################################
 #
 # Logging Configuration
