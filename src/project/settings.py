24c428ffd1a79f7282f864dc45432387be95e836
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-10-21 13:23:54 -0400

    Use a trunk version of django debug toolbar

--- src/project/settings.py
+++ src/project/settings.py
@@ -198,6 +198,7 @@ INTERNAL_IPS = ('127.0.0.1',)
 DEBUG_TOOLBAR_PANELS = (
     'debug_toolbar.panels.version.VersionDebugPanel',
     'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.profiling.ProfilingDebugPanel',
     'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
     'debug_toolbar.panels.headers.HeaderDebugPanel',
     'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
