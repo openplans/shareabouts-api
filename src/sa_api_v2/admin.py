e8b36e2e98554a3e5d87d77beb834e9bd640199a
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-02-08 01:04:58 -0500

    Show only the filter items relevant to the current listed submitted things

--- src/sa_api_v2/admin.py
+++ src/sa_api_v2/admin.py
@@ -4,14 +4,53 @@ via django.contrib.admin.
 """
 
 import models
+from django.contrib.admin import SimpleListFilter
 from django.contrib.gis import admin
 from .apikey.models import ApiKey
+
+
+class SubmissionSetFilter (SimpleListFilter):
+    """
+    Used to filter a list of submissions by type (set name).
+    """
+    title = 'Submission Set'
+    parameter_name = 'set'
+
+    def lookups(self, request, model_admin):
+        qs = model_admin.get_queryset(request)
+        qs = qs.order_by('parent__name').distinct('parent__name').values('parent__name')
+        return [(elem['parent__name'], elem['parent__name']) for elem in qs]
+
+    def queryset(self, request, qs):
+        parent__name = self.value()
+        if parent__name:
+            qs = qs.filter(parent__name=parent__name)
+        return qs
+
+
+class DataSetFilter (SimpleListFilter):
+    """
+    Used to filter a list of submitted things by dataset slug.
+    """
+    title = 'Dataset'
+    parameter_name = 'dataset'
+
+    def lookups(self, request, model_admin):
+        qs = model_admin.get_queryset(request)
+        qs = qs.order_by('dataset__slug').distinct('dataset__slug').values('dataset__slug')
+        return [(elem['dataset__slug'], elem['dataset__slug']) for elem in qs]
+
+    def queryset(self, request, qs):
+        dataset__slug = self.value()
+        if dataset__slug:
+            qs = qs.filter(dataset__slug=dataset__slug)
+        return qs
 
 
 class SubmittedThingAdmin(admin.OSMGeoAdmin):
     date_hierarchy = 'created_datetime'
     list_display = ('id', 'created_datetime', 'updated_datetime', 'submitter_name', 'dataset')
-    list_filter = ('dataset',)
+    list_filter = (DataSetFilter,)
     search_fields = ('submitter__username', 'data',)
 
     raw_id_fields = ('submitter', 'dataset')
@@ -56,7 +95,7 @@ class SubmissionAdmin(SubmittedThingAdmin):
     model = models.Submission
 
     list_display = SubmittedThingAdmin.list_display + ('place', 'set_',)
-    list_filter = ('parent__name',) + SubmittedThingAdmin.list_filter
+    list_filter = (SubmissionSetFilter,) + SubmittedThingAdmin.list_filter
     list_select_related = ('parent',)
     search_fields = ('parent__name',) + SubmittedThingAdmin.search_fields
 
