73bd270344d8db008dcd411d9fe666f2f40d14d9
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-05-06 16:06:43 -0400

    Edit: Revert gevent configuration

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,8 +4,3 @@ x_forwarded_for_header = 'X-FORWARDED-FOR'
 secure_scheme_headers = {
     'X-FORWARDED-PROTO': 'https',
 }
-
-# Enable greenlet support in psycopg
-def post_fork(server, worker):
-    from psycogreen.gevent import patch_psycopg
-    patch_psycopg()
