123128469df21aeaec92cca38e8cd2aa6dd4abd1
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-12-17 13:01:32 -0500

    Correct the API key and origin URL references

--- src/sa_api_v2/admin.py
+++ src/sa_api_v2/admin.py
@@ -135,7 +135,7 @@ class InlineApiKeyAdmin(admin.StackedInline):
             return '(You must save your dataset before you can edit the permissions on your API key.)'
         else:
             return (
-                '<a href="%s"><strong>Edit permissions</strong></a>' % (reverse('admin:apikey_apikey_change', args=[instance.pk]))
+                '<a href="%s"><strong>Edit permissions</strong></a>' % (reverse('admin:sa_api_v2_apikey_change', args=[instance.pk]))
                 + self.permissions_list(instance)
             )
     edit_url.allow_tags = True
@@ -158,7 +158,7 @@ class InlineOriginAdmin(admin.StackedInline):
             return '(You must save your dataset before you can edit the permissions on your origin.)'
         else:
             return (
-                '<a href="%s"><strong>Edit permissions</strong></a>' % (reverse('admin:cors_origin_change', args=[instance.pk]))
+                '<a href="%s"><strong>Edit permissions</strong></a>' % (reverse('admin:sa_api_v2_origin_change', args=[instance.pk]))
                 + self.permissions_list(instance)
             )
     edit_url.allow_tags = True
