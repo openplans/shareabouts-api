ea83f134435e2384a9f3c8210567895827c79cca
Mjumbe Poe <mjumbewu@gmail.com>
2019-04-30 17:06:28 -0400

    Comment out routes that refer to remote social authentication

--- src/sa_api_v2/urls.py
+++ src/sa_api_v2/urls.py
@@ -88,9 +88,9 @@ urlpatterns = patterns('sa_api_v2',
 
     # authentication / association
 
-    url(r'^users/login/error/$', views.remote_social_login_error, name='remote-social-login-error'),
-    url(r'^users/login/(?P<backend>[^/]+)/$', views.remote_social_login, name='remote-social-login'),
-    url(r'^users/logout/$', views.remote_logout, name='remote-logout'),
+    # url(r'^users/login/error/$', views.remote_social_login_error, name='remote-social-login-error'),
+    # url(r'^users/login/(?P<backend>[^/]+)/$', views.remote_social_login, name='remote-social-login'),
+    # url(r'^users/logout/$', views.remote_logout, name='remote-logout'),
 
     url(r'^users/oauth2/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     # url(r'^users/', include('social.apps.django_app.urls', namespace='social')),
