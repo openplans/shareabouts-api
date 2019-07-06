5a1f9350c348bced50294d0699f0c6a00352afa5
Mjumbe Poe <mjumbewu@gmail.com>
2019-07-05 23:26:37 -0400

    Update CI install (environment language, and comments)

--- ci/install.sh
+++ ci/install.sh
@@ -3,25 +3,35 @@
 sudo apt-get update
 
 # libevent development files are required for gevent
+echo
+echo "** Updating libevent"
 sudo apt-get install libevent-dev -y
 
 # Install GeoDjango dependencies -- see
 # https://docs.djangoproject.com/en/dev/ref/contrib/gis/install/#ubuntu
+echo
+echo "** Installing system-level project dependencies"
 sudo apt-get install binutils gdal-bin libgdal-dev libproj-dev \
      postgresql-9.6-postgis-2.3 postgresql-server-dev-9.6 -y
 
 # Update the versions of pip and setup tools that we're using to the latest
+echo
+echo "** Updating pip"
 sudo pip -V
 sudo pip install -U pip setuptools
 sudo pip -V
 
 # Install the python requirements
+echo
+echo "** Installing Python requirements"
 sudo pip install -r requirements.txt
 
 # ... and this, optional testing stuff
 sudo pip install coverage
 
 # Initialize the database
+echo
+echo "** Setting up the test database"
 psql -U postgres <<EOF
     CREATE USER shareabouts WITH PASSWORD 'shareabouts';
     CREATE DATABASE shareabouts;
