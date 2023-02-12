011c2ec303567c62563fc4f75a4ac35c88347967
Mjumbe Poe <mjumbewu@gmail.com>
2023-02-11 22:20:18 -0500

    Upgrade to latest Celery
    This allows us to start to upgrade Django as well.

--- src/project/celery.py
+++ src/project/celery.py
@@ -12,11 +12,14 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 from celery import Celery
 app = Celery('project_wide')
 
-# Using a string here means the worker will not have to
-# pickle the object when using Windows.
-from django.conf import settings
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
 
 
 @app.task(bind=True)
